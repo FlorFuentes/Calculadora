8a24b6220d6879fdcea71471c22b70b93aba5b8f
FlorFuentes <flopy_200412@hotmail.com>
2023-08-31 18:36:35 -0300

    declaracion de variables

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
-alert("Bienvenidos a la calculadora");
+//Variables de numeros seleccionadas por ID
+const num0 = document.getElementById("cero");
+const num1 = document.getElementById("uno");
+const num2 = document.getElementById("dos");
+const num3 = document.getElementById("tres");
+const num4 = document.getElementById("cuatro");
+const num5 = document.getElementById("cinco");
+const num6 = document.getElementById("seis");
+const num7 = document.getElementById("siete");
+const num8 = document.getElementById("ocho");
+const num9 = document.getElementById("nueve");
+
+//Variables de operadores seleccionadas por ID
+const suma = document.getElementById("suma");
+const resta = document.getElementById("resta");
+const multiplicacion = document.getElementById("multiplicacion");
+const division = document.getElementById("division");
+const reset = document.getElementById("reset");
+const igual = document.getElementById("igual");
+const resultado = document.getElementById("resultado");
+
