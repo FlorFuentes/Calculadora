099b5402f81c4d742bac2d2e7b2b7d5b02d2dda3
FlorFuentes <flopy_200412@hotmail.com>
2023-09-14 16:45:00 -0300

    logo

--- js/script.js
+++ js/script.js
@@ -125,4 +125,91 @@ function resolver() {
     }
     resetear();//llamamos a la funcion para resetear los valores de las variables operandoA,operandoB y operando, a 0
     resultado.textContent = res;
+} 
+
+
+//Optimizado,pero no me funciona el boton igual
+/* 
+const numeros = document.querySelectorAll(".numero");
+const operadores = document.querySelectorAll(".operador");
+const resultado = document.getElementById("resultado");
+
+let operandoA = "";
+let operandoB = "";
+let operador = "";
+
+numeros.forEach(numero => {
+    numero.addEventListener("click", (e) => {
+        resultado.textContent += e.target.textContent;
+    });
+});
+
+operadores.forEach(operadorBtn => {
+    operadorBtn.addEventListener("click", (e) => {
+        if (operandoA === "") {
+            operandoA = resultado.textContent;
+            operador = e.target.textContent;
+            limpiar();
+        } else {
+            operandoB = resultado.textContent;
+            resolver();
+            operandoA = resultado.textContent;
+            operador = e.target.textContent;
+            limpiar();
+        }
+    });
+});
+
+document.getElementById("reset").addEventListener("click", () => {
+    resetear();
+});
+
+document.getElementById("igual").addEventListener("click", () => {
+    if (operandoA !== "" && operandoB !== "" && operador !== "") {
+        resolver();
+        resultado.textContent = operandoA; // Muestra el resultado en operandoA
+        operandoB = ""; // Restablece operandoB
+        operador = "";  // Restablece el operador
+    }
+});
+
+function limpiar() {
+    resultado.textContent = "";
 }
+
+function resetear() {
+    resultado.textContent = "";
+    operandoA = "";
+    operandoB = "";
+    operador = "";
+}
+
+function resolver() {
+
+    console.log("OperandoA:", operandoA);
+    console.log("OperandoB:", operandoB);
+    console.log("Operador:", operador);
+
+    let res = 0;
+    switch (operador) {
+        case "+":
+            res = parseFloat(operandoA) + parseFloat(operandoB);
+            break;
+        case "-":
+            res = parseFloat(operandoA) - parseFloat(operandoB);
+            break;
+        case "*":
+            res = parseFloat(operandoA) * parseFloat(operandoB);
+            break;
+        case "/":
+            if (parseFloat(operandoB) === 0) {
+                res = "Error";
+            } else {
+                res = parseFloat(operandoA) / parseFloat(operandoB);
+            }
+            break;
+    }
+    resetear();
+    resultado.textContent=res;
+}
+ */
